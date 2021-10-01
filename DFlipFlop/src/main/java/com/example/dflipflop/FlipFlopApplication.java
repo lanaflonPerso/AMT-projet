1dc42f4782a47dca5dc6869d225f8d3c09c0ee33
TheSnekySnek <diegovillagrasa@gmail.com>
2021-10-01 16:33:19 +0200

    Project Base

---
+++ DFlipFlop/src/main/java/com/example/dflipflop/FlipFlopApplication.java
@@ -0,0 +1,13 @@
+package com.example.dflipflop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipFlopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlipFlopApplication.class, args);
+    }
+
+}
---
+++ DFlipFlop/src/test/java/com/example/dflipflop/FlipFlopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dflipflop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipFlopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
