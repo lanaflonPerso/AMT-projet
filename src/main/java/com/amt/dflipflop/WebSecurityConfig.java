496c4be9d1d839147bf880c47e045ef6e856f2d8
rya-sge <ryan.sauge@heig-vd.ch>
2021-10-17 13:24:44 +0200

    Add code for login in WebSecurityConfig

--- src/main/java/com/amt/dflipflop/WebSecurityConfig.java
+++ src/main/java/com/amt/dflipflop/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.amt.dflipflop;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
@@ -13,8 +18,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "/home", "/", "/css/*", "/js/*", "/images/*", "/demo/*", "login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+        /*http
+                .authorizeRequests()
                 .antMatchers("/**", "/css/*", "/js/*", "/images/*", "/demo/*").permitAll()
                 .anyRequest().authenticated();
-        http.csrf().disable(); // Disable csrf for now
+        http.csrf().disable(); // Disable csrf for now*/
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("username")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        //TODO: Change withDefaultPasswordEncoder
+        return new InMemoryUserDetailsManager(user);
     }
 }
