d1c45b4a402dc31e86bf2cb9e46fa121f1ae942c
rya-sge <ryan.sauge@heig-vd.ch>
2021-10-17 13:18:00 +0200

    Create UserController, UserRepository and class User based on the code from branch ft-login

---
+++ src/main/java/com/amt/dflipflop/Entities/User.java
@@ -0,0 +1,51 @@
+package com.amt.dflipflop.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String username;
+
+    private String email;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String name) {
+        this.username = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
