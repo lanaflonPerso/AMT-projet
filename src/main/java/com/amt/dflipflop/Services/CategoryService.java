f7b01ef296823ffec96dc2089b767490be8bed75
david-pellissier <dav.pellissier@gmail.com>
2021-11-01 11:36:24 +0100

    CategoryRepository & Service

---
+++ src/main/java/com/amt/dflipflop/Services/CategoryService.java
@@ -0,0 +1,40 @@
+package com.amt.dflipflop.Services;
+
+import com.amt.dflipflop.Entities.Category;
+import com.amt.dflipflop.Repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public ArrayList<Category> getAll() {
+
+        Iterable<Category> it = categoryRepository.findAll();
+
+        ArrayList<Category> categories = new ArrayList<Category>();
+        it.forEach(categories::add);
+
+        return categories;
+    }
+
+    public Category get(Integer id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.orElse(null);
+    }
+
+    public Category insert(Category category){
+        return categoryRepository.save(category);
+
+    }
+
+    public Long count() {
+        return categoryRepository.count();
+    }
+}
