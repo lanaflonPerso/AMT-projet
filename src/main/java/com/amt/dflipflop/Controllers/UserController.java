4cce462f01daa5ef09ab76d40ac9deec8bdc04fb
TheSnekySnek <diegovillagrasa@gmail.com>
2021-11-12 16:27:13 +0100

    Added debug info, needs to be removed

--- src/main/java/com/amt/dflipflop/Controllers/UserController.java
+++ src/main/java/com/amt/dflipflop/Controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.amt.dflipflop.Controllers;
 
+import com.amt.dflipflop.Entities.Cart;
+import com.amt.dflipflop.Entities.ProductSelection;
+import com.amt.dflipflop.Services.CartService;
+import com.amt.dflipflop.Services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -7,8 +12,27 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class UserController {
 
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/user/orders")
     public String getUserOrders(Model model) {
+
+        if(cartService.get(0) == null){
+            Cart cart = new Cart();
+            ProductSelection sel = new ProductSelection();
+            sel.setProduct(productService.get(0));
+            cart.addSelection(sel);
+            cartService.insert(cart);
+        }
+        else{
+            Cart userCart = cartService.get(0);
+            userCart.getTotal();
+        }
+
         return "orders";
     }
 
