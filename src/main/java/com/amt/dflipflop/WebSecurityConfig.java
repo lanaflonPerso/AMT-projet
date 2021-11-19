447087ea2fa5e8154b3a585c4efaa03b3a42d90b
rya-sge <ryan.sauge@heig-vd.ch>
2021-11-19 18:05:05 +0100

    Put dataSource in commentary

--- src/main/java/com/amt/dflipflop/WebSecurityConfig.java
+++ src/main/java/com/amt/dflipflop/WebSecurityConfig.java
@@ -17,8 +17,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private DataSource dataSource;
+    /*@Autowired
+    private DataSource dataSource;*/
 
     @Bean
     public UserDetailsService userDetailsService() {
