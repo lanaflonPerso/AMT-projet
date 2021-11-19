d698d7dc23bcd96024b9e825d35289424a7127be
rya-sge <ryan.sauge@heig-vd.ch>
2021-11-19 18:23:37 +0100

    Add jwT class + dependency

--- src/main/java/com/amt/dflipflop/Entities/CustomUserDetailsService.java
+++ src/main/java/com/amt/dflipflop/Entities/CustomUserDetailsService.java
@@ -5,11 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import security.JwtProvider;
+
+import java.util.Optional;
+
+import static org.springframework.security.core.userdetails.User.withUsername;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    JwtProvider jwtProvider;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -19,5 +27,27 @@ public class CustomUserDetailsService implements UserDetailsService {
         }
         return new CustomUserDetails(user);
     }
+
+    /**
+     * Extract username and roles from a validated jwt string.
+     *
+     * @param jwtToken jwt string
+     * @return UserDetails if valid, Empty otherwise
+     */
+    public Optional<UserDetails> loadUserByJwtToken(String jwtToken) {
+        if (jwtProvider.isValidToken(jwtToken)) {
+            return Optional.of(
+                    withUsername(jwtProvider.getUsername(jwtToken))
+                            .authorities(jwtProvider.getRoles(jwtToken))
+                            .password("") //token does not have password but field may not be empty
+                            .accountExpired(false)
+                            .accountLocked(false)
+                            .credentialsExpired(false)
+                            .disabled(false)
+                            .build());
+        }
+        return Optional.empty();
+    }
+
 
 }
